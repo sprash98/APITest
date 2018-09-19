1329c13965bf0f58f51f41cb495ca8b76e124361
sprash98 <sprash98@hotmail.com>
2018-09-19 17:03:33 -0400

    Version 2 of APItester

--- apitester.py
+++ apitester.py
@@ -7,17 +7,27 @@
 
 import requests
 import argparse
-import re
-from collections import OrderedDict
-from telnetlib import STATUS
+import json
+#from collections import OrderedDict
+#from telnetlib import STATUS
 from test_list import TestList
+#from pip._vendor.requests import status_codes
 
 
 class ApiTester(object):
     
     fields= ['firstName', 'lastName', 'username','password','email','phoneNumber']
+    _test_list = [ {
+        "firstName"     : "Prash",
+        "lastName"      : "Raghavan",
+        "email"         : "sprash98@yahoo.com",
+        "phoneNumber"   : "111-222-3333",
+        "username"      : "sprash98",
+        "password"      : "Password234!"    
+        },
+        ]
     
-    def __init__(self,url,data):
+    def __init__(self,url,data=None):
         self.url = url
         self.data = data
         
@@ -34,40 +44,44 @@ class ApiTester(object):
                                 
     
     def post_req(self):
-        status = 0
+        test_pass = 0
+        test_fail = 0
+        test_run = 0
         for data in self.data :
             req = requests.post(self.url,json=data)
             resp_json = req.json()
-            if resp_json['success'] == 'True' :
+            if resp_json['success'] == True :
                 print("User {} creation successful!\n".format(data['username']))
-                status +=1
+                test_pass +=1
             else :
+                test_fail += 1
                 print("User {} creation failed with the following error(s):\n")
                 for err in resp_json['errors']:
                     print("{}\n".format(err))
-        
-        return status
+            test_run +=1
+                    
+        return (test_run,test_fail,test_pass)
     
     
     def validate(self,test):
-        status = 0
-        for k in __class__.fields:
+        result = 0
+        for k in self.fields:
             if k not in test :
                 print("{} missing in test. Cannot add user\n".format(k))
-                return status
-        return (status+1)
+                return result
+        return (result+1)
                         
-        
+    
     def file_parse(self,file):
         
-        status = 0
-        self.tlist = list()
+        result = 0
         try:
             with open(file) as f :
-                self.data = TestList()
+                tests=TestList()
+                tlist = tests.get_cfg()
                 tc = 1
                 failures = list()
-                for t in self.tlist :
+                for t in tlist :
                     if not self.validate(t):
                         print("Test case {} invalid. User cannot be created\n".format(tc))
                         tc +=1
@@ -77,24 +91,55 @@ class ApiTester(object):
                             
         except IOError:
             print("Error accessing file {}. Please verify and try again\n".format(f))
-            return status       
+            return result       
         
         if failures:
-            print("Failed Tests: {}".format(" ".join(map(str,failures))))
+            print("Testcases that failed parsing in file {}: {}".format(" ".join(map(str,failures))),file)
         else:
-            status += 1
-        return status
+            result += 1
+        return result
             
         
-        
-    def arg_parser(self):
+    def main(self):
+        #if __name__ == '__main__':
+        result= 0
+        api_test = ApiTester("http://interview.onforce.com/qe/")
+        #fields= ['firstName', 'lastName', 'username','password','email','phoneNumber']
         parser = argparse.ArgumentParser()
-        parser.add_argument("-f","--file", help="file containing an array of user dictionaries")
+        #parser.add_argument("-f","--file", help="file containing an array of user dictionaries")
+        
         args=parser.parse_args()
-        if args.file :
-            if not self.file_parse(args.file) :
-                print("Unable to parse")
+        if args.testcase :
+            api_test.data = args.testcase
+                
+        else :
+            api_test.data = api_test._test_list
             
+        test_total = len(api_test.data)
+        (run_total,fail_total,pass_total) = self.post_req()
+        
+        print("Test Results:\nTotal Tests: {}, Total Tests Run: {}, Pass: {}, Fail: {}\n".format(test_total,run_total,pass_total,fail_total))
+        return (result+1)    
+        
+                
+if __name__ == '__main__' :
+        #status= 0
+        api_test = ApiTester("http://interview.onforce.com/qe/")
+        #fields= ['firstName', 'lastName', 'username','password','email','phoneNumber']
+        parser = argparse.ArgumentParser()
+        #parser.add_argument("-f","--file", help="file containing an array of user dictionaries")
+        parser.add_argument('-tc', '--testcase', type=json.loads)
+        args=parser.parse_args()
+        if args.testcase :
+            api_test.data = json.loads(args.testcase)
+        else :
+            api_test.data = api_test._test_list
+            
+        test_total = len(api_test.data)
+        (run_total,fail_total,pass_total) = api_test.post_req()
+        
+        print("Test Results:\nTotal Tests: {}, Total Tests Run: {}, Pass: {}, Fail: {}\n".format(test_total,run_total,pass_total,fail_total))
+        #return (status+1)                  
         
         
                                 
